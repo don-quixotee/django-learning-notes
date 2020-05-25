da8a76d68dd9e66c4064ca13befa5635aab08d3f
don-quixotee <Abhishek>
2020-05-25 10:30:21 +0530

    first commit

---
+++ project-setup/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/don_quixote/Desktop/masterDjango/projectDir/myproject_website/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
